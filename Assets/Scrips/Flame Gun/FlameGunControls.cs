bbebcc5b808624c4bf4b4165569b95a0b36b4ec0
Im <imjuday@gmail.com>
2024-11-01 17:04:35 +0700

    Fix FireGun

--- Assets/Scrips/Flame Gun/FlameGunControls.cs
+++ Assets/Scrips/Flame Gun/FlameGunControls.cs
@@ -8,11 +8,25 @@ public class FlameGunControls : MonoBehaviour
     public Transform barrelPos;
     public AudioClip FireSfx;
     public ParticleSystem FireFx;
+    public float fireInterval;
     private AudioSource _audioSourve;
+
+    private bool isShooting;
+    private float fireTimer;
 
     private void Start()
     {
         _audioSourve = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (isShooting && fireTimer <= 0)
+        {
+            Shoot();
+            fireTimer = fireInterval;
+        }
+        fireTimer -= Time.deltaTime;
     }
 
 
@@ -20,7 +34,19 @@ public class FlameGunControls : MonoBehaviour
     {
         Instantiate(FirePrefab, barrelPos.position, barrelPos.rotation);
         _audioSourve.PlayOneShot(FireSfx, 1f);
-        FireFx.Play();
+        
         print("Shoot!");
+    }
+
+    public void OnDeactivate()
+    {
+        FireFx.Stop();
+        isShooting = false;
+    }
+
+    public void OnActivate()
+    {
+        FireFx.Play();
+        isShooting = true;
     }
 }
